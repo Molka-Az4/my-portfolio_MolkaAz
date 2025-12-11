8a247ebf37ca34b3160f710ae94ce2aeef50d769
Molka <molkaazaiez4@gmail.com>
2025-12-11 21:26:59 +0100

    deploy static portfolio

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // <-- this enables static export
+  // other settings you may have
+}
+
+module.exports = nextConfig;
